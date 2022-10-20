3cbb5889b709d93d9a5950a783fe9dca690592e8
Gaurav0927 <54946007+Gaurav0927@users.noreply.github.com>
2022-10-20 19:43:33 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let cel = document.getElementById("cel");
+cel.addEventListener("input",function(){
+
+  let c = this.value;
+  let f = (c * 9/5) + 32;
+
+  if(!Number.isInteger(f)){
+    f = f.toFixed(4);
+  }
+  fah.value = f; 
+
+})
+
+let fah = document.getElementById("fah");
+fah.addEventListener("input", function(){
+
+  let f = this.value;
+  let c = (f - 32) * 5/9;
+
+  if(!Number.isInteger(c)){
+    c = c.toFixed(4);
+  }
+
+  cel.value = c;
+  
+});
